4825e1e51125effc9777d568a36a2f10dd77d96c
İsmail Altay <ismailaltay3553@gmail.com>
2023-03-29 11:25:57 +0300

    Client side of castle settlement is implemented. Castle position data can be reached on Terrain Context. Some UI changes.

--- packages/client/src/pages/game/index.tsx
+++ packages/client/src/pages/game/index.tsx
@@ -2,19 +2,23 @@ import { Grid } from "../../components/TerrainComp/Grid";
 import { useTerrain } from "../../context/TerrainContext";
 import map from '../../../map.json';
 import ScrollContainer from 'react-indiana-drag-scroll';
+import SmallMap from "../../components/GameComp/SmallMap";
 
 function Game() {
-    const { width, height } = useTerrain();
+    const { width, height, isCastleSettled } = useTerrain();
     const values = map;
 
     const terrainStyles = [ 0, 25 ];
-    const smallMap = [1,2];
 
     return (
       <div>
-        <div style={{position:"fixed", zIndex:"1", bottom:"0", left:"0", marginLeft:"2px",marginBottom:"2px"}}>
-          <Grid width={width} height={height} values={values} pixelStyles={smallMap} isBorder={true}/>
-        </div>
+        {
+          !isCastleSettled && 
+          <div style={{position:"absolute", zIndex:"1", color:"white", left:"0", right:"0", top:"0", margin:"100px auto", width:"600px"}}>
+           <h2 className="text-center text-3xl text-white mb-2 border-top border-bottom font-bold">Please settle your castle!</h2>
+          </div>
+        }
+        <SmallMap />
         <div>
           <ScrollContainer className="scroll-container" style={{ zIndex:"0", height:"100vh", overflow:"scroll"}}>
             <Grid width={width} height={height} values={values} pixelStyles={terrainStyles} isBorder={false}/>
